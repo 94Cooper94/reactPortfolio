f674e2c894b01cea42a925d1385ba704b12fdc03
Cooper <eliteswimmer42@gmail.com>
2020-12-10 22:08:26 -0600

    added readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import About from "./components/pages/About";
 import Discover from "./components/pages/Discover";
@@ -7,14 +7,14 @@ import Search from "./components/pages/Search";
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename="/">
       <div>
         <NavTabs />
         <Route exact path="/about" component={About} />
         <Route exact path="/discover" component={Discover} />
         <Route path="/search" component={Search} />
       </div>
-    </HashRouter>
+    </HashRouter >
   );
 }
 
